a90ed8b87d1ee14d8aeaacd2358537471824eeba
Fan Wu <iamnarutowu@gmail.com>
2015-11-17 22:48:02 -0600

    correct

--- P3/d.sql
+++ P3/d.sql
@@ -7,28 +7,31 @@ BEGIN
 	set @num_class = 
 	(select count(*)
 	from requires
-	where A.UoSCode = class_ID);
+	where UoSCode = class_ID);
 
 
 	set @res = (
 	select count(*)
 	from requires, transcript A, uosoffering
 	where requires.UoSCode = class_ID and A.StudId = student_ID and requires.PrereqUoSCode = A.UoSCode and
-	A.grade is not NULL and A.grade != 'F'
+	A.grade is not NULL and A.grade != 'F' and A.UoSCode != class_ID
 	
     and uosoffering.UoSCode = class_ID and uosoffering.year = year_num and uosoffering.Semester = semester_num and uosoffering.MaxEnrollment > uosoffering.Enrollment
-
+	
 	);
     
-    
-    if @num_class = @res
+    if (select count(*) from transcript where StudId= student_ID and UoSCode = class_ID) = 1
+	then 
+		set mode_num = 2;
+        select mode_num;
+    elseif @num_class = @res
     then
 		update uosoffering 
         set Enrollment = Enrollment+1
         where uosoffering.UoSCode = class_ID and uosoffering.Year = year_num and uosoffering.Semester = semester_num;
         
         insert into transcript
-        values (student_ID, class_ID, semester_no, year_num, NULL);
+        values (student_ID, class_ID, semester_num, year_num, NULL);
         
         set mode_num = 1;
         select mode_num;
