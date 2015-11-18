2c3bbd45951c48840a4ddde4498246ce0b15f7a4
Fan Wu <iamnarutowu@gmail.com>
2015-11-18 15:33:25 -0600

    update the trigger

--- P3/e_trigger.sql
+++ P3/e_trigger.sql
@@ -4,7 +4,7 @@ delimiter //
 create trigger belowHalf after update on uosoffering
 for each row
 begin
-	if new.Enrollment < 0.5*new.MaxEnrollment
+	if new.Enrollment < old.Enrollment and new.Enrollment < 0.5*new.MaxEnrollment
     then
 		SIGNAL SQLSTATE 'HY000' 
             SET MESSAGE_TEXT = "check constraint on Enrollment Number. Enrollment Number goes below 50% of the MaxEnrollment." ; 
